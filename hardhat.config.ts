15adebde8520c96564e8e97953c13f2f9527fbd5
KannagiR <136475740+KannagiR@users.noreply.github.com>
2023-07-21 19:03:14 +0530

    hardhat.config.ts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,6 +4,7 @@ import "@nomicfoundation/hardhat-toolbox";
 import "hardhat-circom";
 // circuits
 import circuits = require('./circuits.config.json')
+import 'dotenv/config';
 
 // set env var to the root of the project
 process.env.BASE_PATH = __dirname;
@@ -21,6 +22,12 @@ const config: HardhatUserConfig = {
         version: "0.6.11",
       }
     ]
+  },
+  networks:{
+  goerli: {
+    url: `https://eth-goerli.public.blastapi.io`,
+    accounts: [process.env.GOERLIPRIVATEKEY]
+   }  
   },
   circom: {
     // (optional) Base path for input files, defaults to `./circuits/`
